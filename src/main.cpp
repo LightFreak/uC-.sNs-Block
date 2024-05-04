18b3e1685541d24d91f54449a362cfe28381d3c1
Fabian Goldinger <fgoldinger@gmail.com>
2024-05-04 06:09:35 +0200

    Add Fastled for Signaling

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 #include <Streaming.h>
 #include <MCP23017.h>
+#include <FastLED.h>
 
-MCP23017 MCP(0x27);
+MCP23017 MCP(0x20);
 
 // Definitions
+#define Data_Pin 6
+#define NumberOfLED 4
+CRGB leds[NumberOfLED];
 
 //Sensors Pins: Arduino
-#define reed_Nord 3 
-#define rlt_Nord 4
-#define reed_Sud 5
-#define rlt_Sud 6
+#define reed_Nord A0 
+#define rlt_Nord A1
+#define reed_Sud A2
+#define rlt_Sud A3
 
 // Fahrstrom - Out / Pins: Arduino
 #define relais_Nord 9
@@ -19,6 +23,8 @@ MCP23017 MCP(0x27);
 //Belegtmeldungen 
 boolean belegt_Nord = false;
 boolean belegt_Sud = false;
+boolean freieFahrt_Nord = true;
+boolean freieFahrt_Sud = true;
 
 //Array's
 boolean inputs[6];
@@ -46,6 +52,10 @@ void Init_Pins_Communications(void)
   MCP.pinMode1(FF_Sig_Sud_TX,OUTPUT);
   MCP.pinMode1(FM_Sig_Nord_TX,OUTPUT);
   MCP.pinMode1(FM_Sig_Sud_TX,OUTPUT);
+  MCP.write1(FF_Sig_Nord_TX,HIGH);
+  MCP.write1(FF_Sig_Sud_TX,HIGH);
+  MCP.write1(FM_Sig_Nord_TX,HIGH);
+  MCP.write1(FM_Sig_Sud_TX,HIGH);
 }
 
 void Init_Pins_Outputs(void)
@@ -72,19 +82,24 @@ void set_signal(char side, boolean state)
     if(state) // Frei oder Warnung
     {
       digitalWrite(relais_Nord,HIGH);
+      Serial << "Fahrstrom Nord An" << endl;
       belegt_Nord = false;
+      MCP.write1(FM_Sig_Nord_TX,HIGH);
+      MCP.write1(FF_Sig_Nord_TX,HIGH);
       if(MCP.read1(FF_Sig_Nord_RX))
       {
+        Serial << "Signal Nord: Achtung" << endl;
         //Signal Achtung
       }
       else
       {
+        Serial << "Signal Nord: Freie Fahrt" << endl;
         //Signal Freie Fahrt.
       }
     }
     else // Halt
     {
-
+      Serial << "Signal Nord: Halt" << endl;
     }
   }
   else if(side == 'S') // Südseite
@@ -93,18 +108,23 @@ void set_signal(char side, boolean state)
     {
       digitalWrite(relais_Sud,HIGH);
       belegt_Sud = false;
+      MCP.write1(FM_Sig_Sud_TX,HIGH);
+      MCP.write1(FF_Sig_Sud_TX,HIGH);
+      Serial << "Fahrstrom Süd An" << endl;
       if(MCP.read1(FF_Sig_Sud_RX))
       {
+        Serial << "Signal Süd: Achtung" << endl;  
         //Signal Achtung
       }
       else
       {
+        Serial << "Signal Süd: Freie Fahrt" << endl;  
         //Signal Freie Fahrt.
       }
     }
     else // Halt
     {
-      
+      Serial << "Signal Süd: Halt" << endl;  
     }
   }
 }
@@ -112,54 +132,158 @@ void set_signal(char side, boolean state)
 void setup() {
   Serial.begin(115200);
   Serial << "Begin Setup.." << endl;
+  FastLED.addLeds<NEOPIXEL, Data_Pin>(leds, NumberOfLED);
   Wire.begin();
   MCP.begin();
   Init_Pins_Communications();
   Init_Pins_Inputs();
   Init_Pins_Outputs();
+  for (size_t i = 0; i < NumberOfLED; i++)
+  {
+    leds[i] = CRGB::Red;  /* code */
+  }
+  
+  delay(500);
+
+  for (size_t i = 0; i < NumberOfLED; i++)
+  {
+    leds[i] = CRGB::Yellow;  /* code */
+  }
+  delay(500);
+
+  for (size_t i = 0; i < NumberOfLED; i++)
+  {
+    leds[i] = CRGB::Blue;  /* code */
+  }
+  delay(500);
+  
+  for (size_t i = 0; i < NumberOfLED; i++)
+  {
+    leds[i] = CRGB::Green;  /* code */
+  }
+  delay(500);
+
+  for (size_t i = 0; i < NumberOfLED; i++)
+  {
+    leds[i] = CRGB::Black;  /* code */
+  }
+
   
   Serial << "Setup done" << endl;
 }
 
 void loop() 
 {
-  if (MCP.read1(FM_Sig_Nord_RX))
+  /**
+   * @brief Empfängt Freimeldung auf dem Nordgleis
+   * Ablauf:
+   * - Signal auf Grün Stellen
+   * - Fahrstrom AN
+   * - FM Nord TX auf Active
+   * - FF Nord TX auf Active
+   */
+  if (!MCP.read1(FM_Sig_Nord_RX))
   {
     set_signal('N',true);
+    Serial << "Frei Meldung Nord RX" << endl;
+        
   }
 
-  if(MCP.read1(FM_Sig_Sud_RX))
+   /**
+   * @brief Empfängt Freimeldung auf dem Südgleis
+   * Ablauf:
+   * - Signal auf Grün Stellen
+   * - Fahrstrom AN
+   * - FM Sud TX auf Active
+   * - FF Sud TX auf Active
+   */
+  if(!MCP.read1(FM_Sig_Sud_RX))
   {
     set_signal('S',true);
+    Serial << "FM_Sig_Sud_RX > 1" << endl; 
   }
 
-  if(digitalRead(reed_Nord))
+  /**
+   * @brief Empfängt Freie Fahrt auf dem Nordgleis
+   * Active & nicht Belegt: Grün
+   * Low & nicht Belegt: Achtung.
+   */
+  if(MCP.read1(FF_Sig_Nord_RX) != freieFahrt_Nord)
   {
-    digitalWrite(FF_Sig_Nord_TX,HIGH);
+    Serial << "FF_Sig_Nord_RX > " << MCP.read1(FF_Sig_Sud_RX) << endl; 
+    freieFahrt_Nord = MCP.read1(FF_Sig_Nord_RX);
+    if(!belegt_Nord){
+      set_signal('N',true);
+    }
+  }
+
+  /**
+   * @brief Empfängt Freie Fahrt auf dem Südgleis
+   * Active & nicht Belegt: Grün
+   * Low & nicht Belegt: Achtung.
+   */
+  if(MCP.read1(FF_Sig_Sud_RX) != freieFahrt_Sud)
+  {
+    Serial << "FF_Sig_Sud_RX > " << MCP.read1(FF_Sig_Sud_RX) << endl; 
+    freieFahrt_Sud = MCP.read1(FF_Sig_Sud_RX);
+    if(!belegt_Sud){
+      set_signal('S',true);
+    }
+  }
+
+  /**
+   * @brief Reed auf Nordgleis löst aus.
+   * Ablauf:
+   * - Belegung Speichern
+   * - Signal Rot
+   * - FF Nord Tx auf Inactive
+   */
+  if(!digitalRead(reed_Nord))
+  {
     belegt_Nord = true;
     set_signal('N',false);
-    MCP.write1(FF_Sig_Nord_TX,HIGH);
+    MCP.write1(FF_Sig_Nord_TX,LOW);
+    Serial << "reed Nord > 1" << endl; 
   }
   
-  if(digitalRead(reed_Sud))
+  /**
+   * @brief Reed auf Südgleis löst aus.
+   * Ablauf:
+   * - Belegung Speichern
+   * - Signal Rot
+   * - FF Süd Tx auf Inactive
+   */
+  if(!digitalRead(reed_Sud))
   {
-    digitalWrite(FF_Sig_Sud_TX,HIGH);
     belegt_Sud = true;
     set_signal('S',false);
-    MCP.write1(FF_Sig_Sud_TX,HIGH);
+    MCP.write1(FF_Sig_Sud_TX,LOW);
+    Serial << "Reed sud > 1" << endl; 
   }
 
-  if(digitalRead(rlt_Nord) && belegt_Nord)
+  /**
+   * @brief RLT Nord wird bei Belegung ausgelöst.
+   * Ablauf:
+   * - Fahrstrom aus.
+   */
+  if(!digitalRead(rlt_Nord) && belegt_Nord)
   {
-    set_signal('N',false);
+    //set_signal('N',false);
     digitalWrite(relais_Nord,LOW);
-    MCP.write1(FM_Sig_Nord_TX,LOW);
+    Serial << "Fahrstrom Nord Aus" << endl;
+    //MCP.write1(FM_Sig_Nord_TX,LOW);
   }
 
-  if(digitalRead(rlt_Sud) && belegt_Sud)
+  /**
+   * @brief RLT Süd wird bei Belegung ausgelöst.
+   * Ablauf:
+   * - Fahrstrom aus.
+   */
+  if(!digitalRead(rlt_Sud) && belegt_Sud)
   {
-    set_signal('S',false);
+    //set_signal('S',false);
     digitalWrite(relais_Sud,LOW);
-    MCP.write1(FM_Sig_Sud_TX,LOW);
+    Serial << "Fahrstrom Süd Aus" << endl;
+    //MCP.write1(FM_Sig_Sud_TX,LOW);
   }
 }
